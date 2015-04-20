de440e0d72e54c123281e61c990e9f9a7d6fc698
Flounn <florian.lestic@orange.fr>
2015-04-20 03:13:38 +0200

    books ok

--- includes/bean/books.php
+++ includes/bean/books.php
@@ -1,65 +1,110 @@
-@@ -1,64 +0,0 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- * Description of books
- *
- * @author MinhHieu
- */
 class books {
 
     private $xmlPath;
-    private $domDocument;
+    private $xslDoc;
+    private $xmlDoc;
 
-    public function __construct($xmlPath) {
-         $_xslDoc = new DOMDocument();
-        $_xslDoc->load("includes/xslt/book.xsl");
-        
+    public function __construct() {
+
+        $_xslDoc = new DOMDocument(); 
+		//En production :
+        //$_xslDoc->load("includes/xslt/book.xsl");
+		//$xmlPath = $const_xmlfile;
+		//Pour Florian :
+		$_xslDoc->load("../../xslt/books.xsl");
+		$xmlPath = "../../xml/dblp_prod.xml";
         $_xmlDoc = new DOMDocument();
-        $_xmlDoc->validateOnParse = true;
-        $_xmlDoc->load($xmlPath);
+        $_xmlDoc->load($xmlPath,LIBXML_NOENT | LIBXML_DTDVALID);
 
-        //is this a library xml file? 
-        If ($_xmlDoc->doctype->name != "dblp" ||
-                $_xmlDoc->doctype->systemId != "dblp.dtd") {
-            throw new Exception("Incorrect document type");
-        }
-
-        //is the document valid and well-formed? 
-        if ($_xmlDoc->validate()) {
-            $this->xmlDoc = $_xmlDoc;
-            $this->xslDoc = $_xslDoc;
-            $this->xmlPath = $xmlPath;
-        } else {
-            throw new Exception("Document did not validate");
-        }
+        $this->xmlDoc = $_xmlDoc;
+        $this->xslDoc = $_xslDoc;
+        $this->xmlPath = $xmlPath;
     }
 
     public function __destruct() {
-        // TODO: free memory associated with the DOMDocument
+        unset($this->domDocument);
     }
 
-    public function searchBook($searchArray) {
-        // TODO: return an array with properties of a book 
-    }
-
-    public function addBook($isbn, $title, $author, $genre, $chapters) {
+    public function searchBook($search) {
         $xsltProcessor = new XSLTProcessor();
         $xsltProcessor->registerPHPFunctions();
         $xsltProcessor->importStyleSheet($this->xslDoc);
-        $xsltProcessor->setParameter('', 'booktitle', $search['booktitle']);
-        $xsltProcessor->setParameter('', 'publisher', $search['publisher']);  
-        $result =  $xsltProcessor->transformToXML($this->xmlDoc);
+        $xsltProcessor->setParameter('', 'title', $search['title']);
+        $xsltProcessor->setParameter('', 'author', $search['author']);
+        $xsltProcessor->setParameter('', 'year', $search['year']);
+		$xsltProcessor->setParameter('', 'publisher', $search['publisher']);
+        $xsltProcessor->setParameter('', 'order', $search['order']);
+        $xsltProcessor->setParameter('', 'order_type', $search['order_type']);
+        $xsltProcessor->setParameter('', 'end', $search['end']);
+        $xsltProcessor->setParameter('', 'begin', $search['begin']);
+        $result = $xsltProcessor->transformToXML($this->xmlDoc);
         return $result;
     }
 
-    public function deleteBook($isbn) {
-        // TODO: Delete a book from the library
+    public function updateBook($key, $title,$series,$volume,$pages,$publisher,$isbn) {
+        $xpath = new DOMXPath($this->xmlDoc);
+        $query = "/dblp/book[@key='$key']";
+        $nodeList = $xpath->query($query);
+        if (!$nodeList || $nodeList->length == 0)
+            return false;
+        foreach ($nodeList as $node) {
+            $node->getElementsByTagName('series')[0]->nodeValue = $series;
+            $node->getElementsByTagName('title')[0]->nodeValue = $title;
+			$test = $node->getElementsByTagName('volume');
+			if ($test->length==0){
+				$nod = $this->xmlDoc->createElement('volume');
+				$nod->nodeValue = $volume;
+				$node->appendChild($nod);
+			}
+			else{
+				$node->getElementsByTagName('volume')[0]->nodeValue = $volume;
+			}
+            $test = $node->getElementsByTagName('publisher');
+			if ($test->length==0){
+				$nod = $this->xmlDoc->createElement('publisher');
+				$nod->nodeValue = $publisher;
+				$node->appendChild($nod);
+			}
+			else{
+				$node->getElementsByTagName('publisher')[0]->nodeValue = $publisher;
+			}
+			$test = $node->getElementsByTagName('isbn');
+			if ($test->length==0){
+				$nod = $this->xmlDoc->createElement('isbn');
+				$nod->nodeValue = $isbn;
+				$node->appendChild($nod);
+			}
+			else{
+				$node->getElementsByTagName('isbn')[0]->nodeValue = $isbn;
+			}
+            
+			$test = $node->getElementsByTagName('pages');
+			if ($test->length==0){
+				$nod = $this->xmlDoc->createElement('pages');
+				$nod->nodeValue = $pages;
+				$node->appendChild($nod);
+			}
+			else{
+				$node->getElementsByTagName('pages')[0]->nodeValue = $pages;
+			}
+        }
+        $result = $this->xmlDoc->save($this->xmlPath);
+        return !$result ? $result : true;
     }
+
+    public function deleteBook($key) {
+        $xpath = new DOMXPath($this->xmlDoc);
+        $query = "//dblp/book[@key='$key']";
+        $nodeList = $xpath->query($query);
+        if (!$nodeList || $nodeList->length == 0)
+            return false;
+        foreach ($nodeList as $node)
+            $node->parentNode->removeChild($node);
+        $result = $this->xmlDoc->save($this->xmlPath);
+        return !$result ? $result : true;
+    }
+
 }
